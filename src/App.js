89bacb7c6f77aa9f08f960616c3a6781720f4430
thangndtitf <thangndtitf@gmail.com>
2023-02-26 14:14:15 +0700

    Commit done for Todo Item, add Input and Styles for this 26-2-2023

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
 import { Component } from "react";
 import ToDoItem from "./Components/ToDoItem";
+import downArrow from "../src/img/downArrow.png";
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       items: [
         { title: "Di choi", isactive: false },
-        { title: "Di hoc", isactive: false },
+        { title: "Di hoc", isactive: true },
         { title: "Di lam", isactive: true },
       ],
     };
@@ -32,15 +33,32 @@ class App extends Component {
     };
   }
 
+  onKeyUp(event) {
+    console.log(event);
+  }
   render() {
     console.log(this.state.items);
-    return (
-      <div className="App">
-        {this.state.items.map((item, index) => (
-          <ToDoItem item={item} key={index} onClick={this.onItemClick(item)} />
-        ))}
-      </div>
-    );
+    if (ToDoItem.length) {
+      return (
+        <div className="App">
+          <div className="Header">
+            <img src={downArrow} width={32} height={32}></img>
+            <input
+              type="text"
+              placeholder="Nhan vao thu xem "
+              onKeyUp={this.onKeyUp}
+            ></input>
+          </div>
+          {this.state.items.map((item, index) => (
+            <ToDoItem
+              item={item}
+              key={index}
+              onClick={this.onItemClick(item)}
+            />
+          ))}
+        </div>
+      );
+    }
   }
 }
 
--- src/Components/ToDoItem.js
+++ src/Components/ToDoItem.js
@@ -1,21 +1,30 @@
 import { Component } from "react";
 import classNames from "classnames";
 import "./ToDoItem.css";
+import checkIcon from "../img/check.png";
+import checkIconDone from "../img/check-done.png";
 class ToDoItem extends Component {
   constructor(props) {
     super(props);
   }
 
   render() {
+    var url;
+    if (this.props.item.isactive === false) {
+      url = checkIcon;
+    } else if (this.props.item.isactive === true) {
+      url = checkIconDone;
+    }
+
     return (
       <div
-        className={classNames(
-          { toDoItem: this.props.item.isactive === false },
-          { toDoItem_Done: this.props.item.isactive === true }
-        )}
-        onClick={this.props.onClick}
+        className={classNames("toDoItem", {
+          toDoItem_Done: this.props.item.isactive === true,
+        })}
+        // onClick={this.props.onClick}
       >
-        {this.props.item.title}
+        <img src={url} width={32} height={32} onClick={this.props.onClick} />
+        <p>{this.props.item.title}</p>
       </div>
     );
   }
